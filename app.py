3487d8cf40baf240b521ac282c966e2b069a99cb
Josef Utbult <josef@utbult.design>
2021-09-01 00:46:35 +0200

    Add skeleton framework

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # app.run(ssl_context='adhoc', host='0.0.0.0', port='443')
-    app.run(host='0.0.0.0', port='80')
+    # app.run(host='0.0.0.0', port='80')
+    app.run()
